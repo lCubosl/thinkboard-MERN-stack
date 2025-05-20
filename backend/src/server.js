4bb4a1e5d9ae3ebc68173a4b95dc064528bf8684
LuisM <muislarques@gmail.com>
2025-05-20 01:06:12 +0100

    server.js updates: ratelimiter using upstash ratelimit and redis, information logs to console upstash.js: rateLimit configuration rateLimiter.js: implementation of previous configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
+import ratelimiter from "./middleware/rateLimiter.js"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -8,11 +9,20 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 
-connectDB()
-
+// middleware allows parsing of json bodies: req.body
 app.use(express.json())
+// middleware to ratelimit requests to server using upstash ratelimit & redis
+app.use(ratelimiter)
+// middleware logs method and url
+app.use((req, res, next) => {
+  console.log(`req method: ${req.method}, req URL: ${req.url}`)
+  next()
+})
+
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
-  console.log("Server started on PORT:", PORT)
+connectDB().then(() => {
+  app.listen(5001, () => {
+    console.log("Server started on PORT:", PORT)
+  })
 })
